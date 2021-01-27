cf2f49235f4001826a3bb6845d78d27b02b924ac
CRosenauer <christopherrosenauer101@gmail.com>
2021-01-26 20:17:22 -0800

    More Input Stuff

--- src/Entity/Components/InputComponent.gd
+++ src/Entity/Components/InputComponent.gd
@@ -2,12 +2,42 @@ extends "res://Entity/Base/IComponent.gd"
 
 const INPUTS = preload("res://Inputs/InputMap.gd")
 
-var m_inputArray = [0, 0, 0, 0, 0, 0, 0]
-var m_edgeArray  = [0, 0, 0, 0, 0, 0, 0]
+var m_inputArray = [0, 0, 0, 0, 0]
+var m_edgeArray  = [0, 0, 0, 0, 0]
 
 
 func QueryInputs():
-	pass
+	ClearInputs()
+	
+	#Cardinal Direction
+	if(Input.is_action_pressed("InputDown")):
+		m_inputArray[INPUTS.Input_Down] += 1
+	if(Input.is_action_pressed("InputUp")):
+		m_inputArray[INPUTS.Input_Down] -= 1
+	
+	if(Input.is_action_pressed("InputRight")):
+		m_inputArray[INPUTS.Input_Left] += 1
+	if(Input.is_action_pressed("InputLeft")):
+		m_inputArray[INPUTS.Input_Right] -= 1
+	
+	#Actions
+	InterpretAction(INPUTS.Input_Jump, "InputJump")
+	InterpretAction(INPUTS.Input_Grapple, "InputGrapple")
+	InterpretAction(INPUTS.Input_Dash, "InputDash")
+
+func ClearInputs():
+	for i in range(0, INPUTS.__SIZE__):
+		m_inputArray[i] = 0
+		m_edgeArray[i]  = 0
+
+func InterpretAction(enumValue, inputValue):
+	if(Input.is_action_pressed(inputValue)):
+		if(m_inputArray[enumValue] == 0):
+			m_edgeArray[enumValue] += 1 #Cant use in engine rising in falling edges cause we arent using events lul.
+		m_inputArray[enumValue] = 1
+	else:
+		if(m_inputArray[enumValue] == 1):
+			m_edgeArray[enumValue] -= 1
 
 func GetInputs():
 	return m_inputArray
--- src/Inputs/InputMap.gd
+++ src/Inputs/InputMap.gd
@@ -1,4 +1,4 @@
 extends Node
 
 
-enum { Input_Up, Input_Down, Input_Left, Input_Right, Input_Jump, Input_Dash, Input_Grapple, __SIZE__ }
+enum { Input_Down, Input_Right, Input_Jump, Input_Dash, Input_Grapple, __SIZE__ }
